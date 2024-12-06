3b83dc5faf48febe3f6e221e59500ec7e66c20f5
ella-mzg <ella.mzgh@gmail.com>
2024-12-06 23:14:03 +0100

    resolve ESLint errors

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -4,30 +4,27 @@ import FormField from "@/components/ui/FormField"
 import { Field, Formik } from "formik"
 
 const initialValues = { title: "" }
-
-const TaskForm = ({ onSubmit }) => {
-  return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={(values, { resetForm }) => {
-        onSubmit(values)
-        resetForm()
-      }}>
-      {() => (
-        <Form>
-          <FormField
-            name="title"
-            label="Task Title"
-            placeholder="Enter task title"
-            as={Field}
-          />
-          <Button type="submit" variant="primary">
-            Add Task
-          </Button>
-        </Form>
-      )}
-    </Formik>
-  )
-}
+const TaskForm = ({ onSubmit }) => (
+  <Formik
+    initialValues={initialValues}
+    onSubmit={(values, { resetForm }) => {
+      onSubmit(values)
+      resetForm()
+    }}>
+    {() => (
+      <Form>
+        <FormField
+          name="title"
+          label="Task Title"
+          placeholder="Enter task title"
+          as={Field}
+        />
+        <Button type="submit" variant="primary">
+          Add Task
+        </Button>
+      </Form>
+    )}
+  </Formik>
+)
 
 export default TaskForm
