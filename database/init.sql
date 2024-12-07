4f4ca45dc9a1551fc4adf1b2f1f18d3713b65eb1
ella-mzg <ella.mzgh@gmail.com>
2024-12-07 02:14:25 +0100

    add validation for task title and limit db field to 60 char - See #11

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    title VARCHAR(60) NOT NULL,
     completed BOOLEAN DEFAULT FALSE
 );
 
--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -2,11 +2,20 @@ import Button from "@/components/ui/Button"
 import Form from "@/components/ui/Form"
 import FormField from "@/components/ui/FormField"
 import { Field, Formik } from "formik"
+import { object, string } from "yup"
 
 const initialValues = { title: "" }
+const validationSchema = object({
+  title: string()
+    .trim()
+    .min(1, "Task title must contain at least 1 character")
+    .max(60, "Task title must be 60 characters or less")
+    .required("Task title is required")
+})
 const TaskForm = ({ onSubmit }) => (
   <Formik
     initialValues={initialValues}
+    validationSchema={validationSchema}
     onSubmit={(values, { resetForm }) => {
       onSubmit(values)
       resetForm()
