075b2d6763ccd84c426e3fed9fdef18a7d03e720
Ella Mzoughi <ella.mzgh@gmail.com>
2024-12-18 19:51:32 +0000

    feat: add frontend service

---
+++ frontend/src/pages/_document.jsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from "next/document"
+
+const Document = () => (
+  <Html lang="en">
+    <Head />
+    <body className="bg-gray-100 text-gray-900">
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
+
+export default Document
