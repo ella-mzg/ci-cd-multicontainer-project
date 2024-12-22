ef0bea30f2c510f197afcb6726a8e2c3434fc1fd
Ella Mzoughi <ella.mzgh@gmail.com>
2024-12-22 12:21:58 +0000

    Revert "Merge branch 'feat/11-frontend-service' into 'main'"
    This reverts merge request !5

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Task(db.Model):
-    __tablename__ = "tasks"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(60), nullable=False)
-    completed = db.Column(db.Boolean, default=False)
