16d1d735ce09e78b61a7420aa787fb8d08b335d2
ella-mzg <ella.mzgh@gmail.com>
2024-12-23 18:50:49 +0100

    trigger pipeline - See #23

--- backend/tests/test_tasks.py
+++ backend/tests/test_tasks.py
@@ -70,3 +70,4 @@ def test_delete_non_existent_task(client):
     response = client.delete("/api/tasks/4")
     assert response.status_code == 404
     assert response.get_json() == {"error": "Task not found"}
+# tests
