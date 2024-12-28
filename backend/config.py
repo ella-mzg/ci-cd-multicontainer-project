70a419d29ea448d8ea9da80eef1d345d8817d072
ella-mzg <ella.mzgh@gmail.com>
2024-12-28 17:33:26 +0100

    fix backend config - Fixes #25

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,8 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
 class Config:
     try:
         DATABASE_URL = os.getenv("DATABASE_URL")
+        if not DATABASE_URL:
+            raise ValueError("DATABASE_URL is not set or is empty")
     except Exception as e:
         print(f"Error loading database configuration: {e}")
         DATABASE_URL = "sqlite:///:memory:"
