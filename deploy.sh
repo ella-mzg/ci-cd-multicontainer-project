be8f45fea6b981b6137c9e0ba6f90733ba9670c7
ella-mzg <ella.mzgh@gmail.com>
2024-12-30 23:04:38 +0100

    debug scripts - See #17

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 VM_USER="${VM_USER:-}"
 VM_HOST="${VM_HOST:-}"
-PROJECT_NAME= "ci-cd-multicontainer-project"
+PROJECT_NAME="ci-cd-multicontainer-project"
 TARGET_DIR="/home/$VM_USER/$PROJECT_NAME"
 
-if [ -z "$VM_HOST" ]; then
-    echo "VM_HOST is not set. Exiting."
+if [ -z "$VM_HOST" ] || [ -z "$VM_USER" ]; then
+    echo "VM_HOST or VM_USER is not set. Exiting."
     exit 1
 fi
 
@@ -17,9 +17,9 @@ rsync -avz --exclude='.git/' --exclude='node_modules/' --exclude='venv/' ./ "$VM
 }
 
 echo "Connecting to $VM_USER@$VM_HOST to start deployment..."
-ssh -i ~/.ssh/id_rsa "$VM_USER@$VM_HOST" << 'EOF'
+ssh -i ~/.ssh/id_rsa "$VM_USER@$VM_HOST" << EOF
 set -e
-cd /home/$VM_USER/code || { echo "Project directory not found"; exit 1; }
+cd $TARGET_DIR || { echo "Project directory not found"; exit 1; }
 docker-compose down || { echo "Failed to stop running containers"; exit 1; }
 docker-compose up -d || { echo "Failed to start containers"; exit 1; }
 echo "Deployment completed successfully!"
