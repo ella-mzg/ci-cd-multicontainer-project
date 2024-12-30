5def4f9058a642d2849d7f738de70c652ddc6743
ella-mzg <ella.mzgh@gmail.com>
2024-12-30 22:50:34 +0100

    fix host variable name - See #17

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 VM_USER="${VM_USER:-}"
-VM_IP="${VM_IP:-}"
+VM_HOST="${VM_HOST:-}"
 PROJECT_NAME= "ci-cd-multicontainer-project"
 TARGET_DIR="/home/$VM_USER/code/$PROJECT_NAME"
 
-if [ -z "$VM_IP" ]; then
-    echo "VM_IP is not set. Exiting."
+if [ -z "$VM_HOST" ]; then
+    echo "VM_HOST is not set. Exiting."
     exit 1
 fi
 
 echo "Starting deployment..."
 
-echo "Syncing files to $VM_USER@$VM_IP:$TARGET_DIR..."
-rsync -avz --exclude='.git/' --exclude='node_modules/' --exclude='venv/' ./ "$VM_USER@$VM_IP:$TARGET_DIR/" || {
+echo "Syncing files to $VM_USER@$VM_HOST:$TARGET_DIR..."
+rsync -avz --exclude='.git/' --exclude='node_modules/' --exclude='venv/' ./ "$VM_USER@$VM_HOST:$TARGET_DIR/" || {
     echo "File sync failed. Exiting."
     exit 1
 }
 
-echo "Connecting to $VM_USER@$VM_IP to start deployment..."
-ssh -i ~/.ssh/id_rsa "$VM_USER@$VM_IP" << 'EOF'
+echo "Connecting to $VM_USER@$VM_HOST to start deployment..."
+ssh -i ~/.ssh/id_rsa "$VM_USER@$VM_HOST" << 'EOF'
 set -e
 cd /home/$VM_USER/code || { echo "Project directory not found"; exit 1; }
 docker-compose down || { echo "Failed to stop running containers"; exit 1; }
